e2f34d70dda60810c7f4554501dd0066b672f599
athmaw <athenajamieuy@gmail.com>
2026-04-12 23:59:01 +0800

    to be reviewed

--- marioKart.c
+++ marioKart.c
@@ -62,55 +62,174 @@ typedef struct{
 void initList(VirtualList *R);
 int allocSpace(VirtualList *R);
 void deallocSpace(VirtualList *R, int Ndex);
-void insertFirst(int *L, VirtualList *R, int elem);
-void insertLast(int *L, VirtualList *R, int elem);
-void insertPos(int *L, VirtualList *R, int elem);
-void insertSorted(int *L, VirtualList *R, int elem);
-void delete(int *L, VirtualList *R, int elem);
-void deleteAllOccurrence(int *L, VirtualList *R, int elem);
+void insertFirst(int *L, VirtualList *R, Player elem);
+void insertLast(int *L, VirtualList *R, Player elem);
+void insertPos(int *L, VirtualList *R, Player elem);
+void insertSorted(int *L, VirtualList *R, Player elem);
+void delete(int *L, VirtualList *R, String ID);
+void deleteAllOccurrence(int *L, VirtualList *R, String ID);
 void display(int L, VirtualList R);
 
 int main() {
+        VirtualList R;
+        raceList RL;
     
-    return 0;
+        initList(&R);
+        RL.LIST = -1;
+        RL.count = 0;
+    
+        Player p1;
+        strcpy(p1.account.ID, "2345");
+        strcpy(p1.account.name, "Mario");
+        p1.currCoins = 10;
+
+        Player p2;
+        strcpy(p2.account.ID, "0021");
+        strcpy(p2.account.name, "Luigi");
+        p2.currCoins = 5;
+
+        insertFirst(&RL.LIST, &R, p1);
+        insertFirst(&RL.LIST, &R, p2);
+        display(RL.LIST, R);
+    
+        return 0;
 }
 
 void initList(VirtualList *R) {
-    
+    int i;
+
+    for(i = 0; i < MAX - 1; i++) {
+        R->R[i].next = i + 1;
+    }
+    R->R[MAX - 1].next = -1;
+    R->Avail = 0;
 }
 
 int allocSpace(VirtualList *R) {
-    
+    int Ndex;
+
+    if(R->Avail == -1) {
+        return -1;
+    }
+    Ndex = R->Avail;
+    R->Avail = R->R[Ndex].next;
+    R->R[Ndex].next = -1;
+
+    return Ndex;
 }
 
 void deallocSpace(VirtualList *R, int Ndex) {
-    
+    R->R[Ndex].next = R->Avail;
 }
 
-void insertFirst(int *L, VirtualList *R, int elem) {
-    
+void insertFirst(int *L, VirtualList *R,  Player elem) {
+    int Ndex = allocSpace(R);
+
+    if(Ndex != -1) {
+        R->R[Ndex].racer = elem;
+        R->R[Ndex].next = *L;
+        *L = Ndex;
+    }
 }
 
-void insertLast(int *L, VirtualList *R, int elem) {
+void insertLast(int *L, VirtualList *R, Player elem) {
+    int Ndex = allocSpace(R);
     
+    if(Ndex != -1) {
+        R->R[Ndex].racer = elem;
+        R->R[Ndex].next = -1;
+
+        if(*L == -1) {
+            *L = Ndex;
+        } else{
+            int i = *L;
+
+            while(R->R[i].next != -1) {
+                i = R->R[i].next;
+            }
+            R->R[i].next = Ndex;
+        }
+    }
 }
 
-void insertPos(int *L, VirtualList *R, int elem) {
-    
+void insertPos(int *L, VirtualList *R, Player elem) {
+    int Ndex = allocSpace(R);
+
+    if(Ndex != -1) {
+        R->R[Ndex].racer = elem;
+
+        if(*L == -1 || R->R[*L].racer.currCoins  >= elem.currCoins) {
+            R->R[Ndex].next = *L;
+            *L = Ndex;
+        } else{
+            int i = *L;
+
+            while(R->R[i].next != -1 && R->R[R->R[i].next].racer.currCoins < elem.currCoins) {
+                i = R->R[i].next;
+            }
+            R->R[Ndex].next = R->R[i].next;
+            R->R[i].next = Ndex;
+        }
+    }
 }
 
-void insertSorted(int *L, VirtualList *R, int elem) {
-    
+void insertSorted(int *L, VirtualList *R, Player elem) {
+    int Ndex = allocSpace(R);
+
+    if(Ndex != -1) {
+        R->R[Ndex].racer = elem;
+
+        if(*L == -1 || R->R[*L].racer.currCoins >= elem.currCoins) {
+            R->R[Ndex].next = *L;
+            *L = Ndex;
+        } else{
+            int i = *L;
+
+            while(R->R[i].next != -1 && R->R[R->R[i].next].racer.currCoins < elem.currCoins) {
+                i = R->R[i].next;
+            }
+            R->R[Ndex].next = R->R[i].next;
+            R->R[i].next = Ndex;
+        }
+    }
 }
 
-void delete(int *L, VirtualList *R, int elem) {
-    
+void delete(int *L, VirtualList *R, String ID) {
+    int *trav;
+
+    for(trav = L; *trav != -1 && strcmp(R->R[*trav].racer.account.ID, ID) != 0; trav = &(R->R[*trav].next)) {}
+    if(*trav != -1) {
+        int temp = *trav;
+
+        *trav = R->R[temp].next;
+        deallocSpace(R, temp);
+    }
 }
 
-void deleteAllOccurrence(int *L, VirtualList *R, int elem) {
-    
+void deleteAllOccurrence(int *L, VirtualList *R, String ID) {
+    int *trav = L;
+
+    while(*trav != -1) {
+        if(strcmp(R->R[*trav].racer.account.ID, ID) == 0) {
+            int temp = *trav;
+
+            *trav = R->R[temp].next;
+            deallocSpace(R, temp);
+        } else{
+            trav = &R->R[*trav].next;
+        }
+    }
 }
 
 void display(int L, VirtualList R) {
-    
+    int trav;
+
+    printf("\n--- Current Race Standings ---\n");
+    for(trav = L; trav != -1; trav = R.R[trav].next) {
+        printf("ID: #%s | Name: %10s | Coins: %d\n", 
+            R.R[trav].racer.account.ID, 
+            R.R[trav].racer.account.name, 
+            R.R[trav].racer.currCoins);
+    }
+    printf("------------------------------\n");
 }
